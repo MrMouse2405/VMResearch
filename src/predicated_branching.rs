8012a7c6c2e5e06a458af80770062bcd5361c139
mrmouse2405 <mrmouse2405@gmail.com>
2024-09-20 07:12:23 -0400

    Report 1

---
+++ src/predicated_branching.rs
@@ -0,0 +1,76 @@
+/*
+    VM Research
+
+    Author: MrMouse2405
+    Date:   Sep 13,2024
+*/
+
+use crate::bytecode::ByteCode;
+
+#[inline(always)]
+fn inc(v: i32) -> i32 {
+    v + 1
+}
+
+fn inc_dec(v: &mut i32, dec: fn(i32) -> i32) {
+    *v += 1;
+}
+
+#[inline(always)]
+fn dec(v: i32) -> i32 {
+    v - 1
+}
+
+#[inline(always)]
+fn neg(v: i32) -> i32 {
+    -v
+}
+
+#[inline(always)]
+fn mul2(v: i32) -> i32 {
+    v * 2
+}
+
+#[inline(always)]
+fn div2(v: i32) -> i32 {
+    v / 2
+}
+
+#[inline(always)]
+fn add7(v: i32) -> i32 {
+    v + 7
+}
+
+#[inline(always)]
+fn empty(v: i32) -> i32 {
+    v
+}
+
+pub fn interpreter_predicated_init(code: &Vec<ByteCode>) -> Vec<Box<dyn Fn(i32) -> i32>> {
+    let mut vm_code: Vec<Box<dyn Fn(i32) -> i32>> = Vec::with_capacity(code.len());
+
+    for bytecode in code {
+        match bytecode {
+            ByteCode::INC => vm_code.push(Box::new(inc)),
+            ByteCode::DEC => vm_code.push(Box::new(dec)),
+            ByteCode::MUL2 => vm_code.push(Box::new(mul2)),
+            ByteCode::DIV2 => vm_code.push(Box::new(div2)),
+            ByteCode::ADD7 => vm_code.push(Box::new(add7)),
+            ByteCode::NEG => vm_code.push(Box::new(neg)),
+            ByteCode::EMPTY => vm_code.push(Box::new(empty)),
+        }
+    }
+
+    vm_code
+}
+
+pub fn interpeter_predicated(vm_code: &Vec<Box<dyn Fn(i32) -> i32>>, init_val: i32) -> i32 {
+    let mut val = init_val;
+
+    for instructions in vm_code {
+        val = instructions(val);
+    }
+
+    // incase there is zero code
+    return val;
+}
